7e6d524c1fe101a33326c957aa74036d11419ebc
Felipe Andrade <feandrad@gmail.com>
2022-01-30 17:43:38 -0300

    layout

--- app/src/main/java/io/felipeandrade/reddit/ui/StartFragment.kt
+++ app/src/main/java/io/felipeandrade/reddit/ui/StartFragment.kt
@@ -1,4 +1,4 @@
-package io.felipeandrade.reddit.ui.topposts
+package io.felipeandrade.reddit.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,19 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import io.felipeandrade.reddit.databinding.FragmentPostsBinding
+import io.felipeandrade.reddit.databinding.FragmentStartBinding
+import io.felipeandrade.reddit.ui.topposts.TopPostsViewModel
 
-class TopPostsFragment: Fragment() {
+class StartFragment : Fragment() {
 
     private val sharedViewModel: TopPostsViewModel by activityViewModels()
-    private val binding by lazy { FragmentPostsBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentStartBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding.viewModel = sharedViewModel
+
         return binding.root
     }
 }
--- app/src/main/java/io/felipeandrade/reddit/ui/topposts/ReadPostFragment.kt
+++ app/src/main/java/io/felipeandrade/reddit/ui/topposts/ReadPostFragment.kt
@@ -1,10 +1,26 @@
 package io.felipeandrade.reddit.ui.topposts
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import io.felipeandrade.reddit.databinding.FragmentPostsBinding
+import io.felipeandrade.reddit.databinding.FragmentReadPostBinding
 
 class ReadPostFragment : Fragment() {
 
     private val sharedViewModel: TopPostsViewModel by activityViewModels()
+    private val binding by lazy { FragmentReadPostBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding.viewModel = sharedViewModel
+        return binding.root
+    }
 
 }
